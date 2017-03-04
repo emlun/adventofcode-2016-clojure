a71a41c583eb9b8bbc2ed4efe67be72d75cf5422
Emil Lundberg <lundberg.emil@gmail.com>
2017-03-04 23:14:24 +0100

    Change license to GPL

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject adventofcode-2016-clojure "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "GNU General Public License, version 3 or higher"
+            :url "https://www.gnu.org/licenses/gpl.txt"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot adventofcode-2016.core
   :target-path "target/%s"
