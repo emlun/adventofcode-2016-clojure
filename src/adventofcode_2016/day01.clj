b56f376e5441804e65d73208adbd4dfae7c8b70d
Emil Lundberg <lundberg.emil@gmail.com>
2017-03-04 21:15:34 +0100

    Solve day 1 a

--- src/adventofcode_2016/day01.clj
+++ src/adventofcode_2016/day01.clj
@@ -1,9 +1,67 @@
 (ns adventofcode-2016.day01
   (:gen-class)
+  (:require [clojure.string])
+  (:require [adventofcode-2016.math :refer (abs)])
+)
+
+(defn to-turn [word]
+  (case (first word)
+    \L :left
+    \R :right
+  )
+)
+
+(defn rotate
+  [dir turn]
+  { :pre [(map? dir) (number? (:x dir)) (number? (:y dir)) (keyword? turn)] }
+  (case turn
+    :left { :x (:y dir) :y (- (:x dir)) }
+    :right  { :x (- (:y dir)) :y (:x dir) }
+  )
+)
+
+(defn walk [pos dir dist]
+  (merge-with (fn [p d] (+ p (* d dist))) pos dir )
+)
+
+(defn walk-step [state step]
+  (let [ new-dir (rotate (:dir state) (:turn step)) ]
+    {
+      :dir new-dir
+      :pos (walk (:pos state) new-dir (:dist step) )
+    }
+  )
+)
+
+(defn extract-steps [line]
+  (map
+    (fn [word]
+      { :turn (to-turn word)
+        :dist (read-string (apply str (rest word))) }
+    )
+    (clojure.string/split (clojure.string/trim line) #"\s+")
+  )
+)
+
+(def start-state
+  {
+    :pos { :x 0 :y 0 }
+    :dir { :x 1 :y 0 }
+  }
+)
+(defn find-bunny-hq
+  [lines]
+  (let [ steps (mapcat extract-steps lines) ]
+    (map second (seq (:pos (reduce walk-step start-state steps))))
+  )
 )
 
 (defn solve
-  "Solve day 1 of Advent of Code 2016"
+  "solve day 1 of Advent of Code 2016"
   [input-lines & args]
-  input-lines
+  (do
+    (def bunny-hq (find-bunny-hq input-lines))
+    (println "Location of Bunny HQ:" bunny-hq)
+    (println "Distance:" (apply + (map abs bunny-hq)))
+  )
 )
