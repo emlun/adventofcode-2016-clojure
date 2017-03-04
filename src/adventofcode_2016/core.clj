1baac4f0ea378b308fac70bc5643ea12da1a497b
Emil Lundberg <lundberg.emil@gmail.com>
2017-03-04 18:36:52 +0100

    Add scaffold for general entry point

--- src/adventofcode_2016/core.clj
+++ src/adventofcode_2016/core.clj
@@ -1,7 +1,16 @@
 (ns adventofcode-2016.core
-  (:gen-class))
+  (:gen-class)
+  (:require clojure.string)
+  (:require adventofcode-2016.day01)
+)
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  "run the solver for the given day with lines from standard input as argument"
+  [day & args]
+  (do
+    (def padded-day (if (< 1 (count day)) day (str "0" day)))
+    (def lines (clojure.string/split-lines (slurp *in*)))
+    (def solve (resolve (symbol (str "adventofcode-2016.day" padded-day) "solve")))
+    (println (solve lines))
+  )
+)
---
+++ src/adventofcode_2016/day01.clj
@@ -0,0 +1,9 @@
+(ns adventofcode-2016.day01
+  (:gen-class)
+)
+
+(defn solve
+  "Solve day 1 of Advent of Code 2016"
+  [input-lines & args]
+  input-lines
+)
