2f2ad9bcb59bc738f576e80356c25d0b4fe136b6
Emil Lundberg <lundberg.emil@gmail.com>
2017-03-10 20:17:55 +0100

    Make run-all main dynamically find all day solvers

--- src/adventofcode_2016/core.clj
+++ src/adventofcode_2016/core.clj
@@ -3,20 +3,30 @@
   (:require clojure.string)
 )
 
-(defn pad [day] (if (< 1 (count day)) day (str "0" day)))
+(defn pad [day]
+  (if (number? day)
+    (recur (str day))
+    (if (< 1 (count day)) day (str "0" day))))
+
+(defn day-namespace [day] (symbol (str "adventofcode-2016.day" (pad day))))
 
 (defn -main
   "Run the solver for the given day (or all) with lines from file as argument. If file is -, use standard input; if not given, use default."
   ([]
-    (doseq [day (range 1 2)]
-      (println "\n===" "Day" day "===\n")
-      (-main (str day))
+    (try
+      (loop [day 1]
+        (require (day-namespace day))
+        (println "\n===" "Day" day "===\n")
+        (-main day)
+        (recur (inc day))
+      )
+      (catch java.io.FileNotFoundException e ())
     )
   )
   ([day] (-main day (str "resources/day" (pad day) ".in")))
   ([day file & args]
     (do
-      (def dayspace (symbol (str "adventofcode-2016.day" (pad day))))
+      (def dayspace (day-namespace day))
       (require dayspace)
       (def input (if (= "-" file) *in* file))
       (def lines (clojure.string/split-lines (slurp input)))
