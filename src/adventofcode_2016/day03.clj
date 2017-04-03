ed831004f132f393611c6280beb1693c3441cfee
Emil Lundberg <lundberg.emil@gmail.com>
2017-04-03 23:27:23 +0200

    Extract transpose function to util module

--- src/adventofcode_2016/day03.clj
+++ src/adventofcode_2016/day03.clj
@@ -1,25 +1,7 @@
 (ns adventofcode-2016.day03
   (:require clojure.string)
-  (:require [adventofcode-2016.util :refer (flip grouped)])
+  (:require [adventofcode-2016.util :refer (flip grouped transpose)])
 )
-
-(defn transpose
-  "Transform seq [[a1 a2 ...] [b1 b2 ...] ...] to seq [[a1 b1 ...] [a2 b2 ...] ...]"
-  {
-   :test #(do
-            (assert (= (transpose []) (transpose [[]]) (transpose [[] []]) []), "Transpose of empty input is empty")
-            (assert (= (transpose [[1]]) [[1]]), "Transpose of single element is identity function")
-            (assert (= (transpose [[1 2]]) [[1] [2]]), "Transpose of two-column row is two one-column rows")
-            (assert (= (transpose [[1 2] [3 4]]) [[1 3] [2 4]]), "Transpose of 2x2 matrix is matrix transpose")
-            (assert (= (transpose [[1 2] []]) []), "Rows are truncated to shortest row")
-            (assert (= (transpose [[1 2] [3]]) [[1 3]]), "Rows are truncated to shortest row")
-            )
-   }
-  [seqs]
-  (if (seq seqs)
-    (apply map (cons vector seqs))
-    ()
-  ))
 
 (defn possible-triangles [triangles]
   (->> triangles
