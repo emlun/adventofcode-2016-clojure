63f336cd6662c8d7954fd391fa3b20edcc70caf4
Emil Lundberg <lundberg.emil@gmail.com>
2017-04-03 23:25:26 +0200

    Extract grouped function to util module

--- src/adventofcode_2016/day03.clj
+++ src/adventofcode_2016/day03.clj
@@ -1,31 +1,7 @@
 (ns adventofcode-2016.day03
   (:require clojure.string)
+  (:require [adventofcode-2016.util :refer (grouped)])
 )
-
-(defn grouped
-  "Return elements in x in groups of n"
-  {
-    :test #(do
-             (assert (= [] (grouped 5 [])))
-             (assert (= [] (grouped 5 [])))
-             (assert (= [[0] [1] [2]] (grouped 1 [0 1 2])))
-             (assert (= [[0]] (grouped 3 [0])))
-             (assert (= [[0 1]] (grouped 3 [0 1])))
-             (assert (= [[0 1 2]] (grouped 3 [0 1 2])))
-             (assert (= [[0 1 2] [3]] (grouped 3 [0 1 2 3])))
-             )
-  }
-  [n x]
-  (loop [result [] remain x]
-    (cond
-      (= n (count result))
-        (cons result (grouped n remain))
-      (seq remain)
-        (recur (conj result (first remain)) (rest remain))
-      (seq result)
-        (conj [] result)
-      :else []
-    )))
 
 (defn transpose
   "Transform seq [[a1 a2 ...] [b1 b2 ...] ...] to seq [[a1 b1 ...] [a2 b2 ...] ...]"
