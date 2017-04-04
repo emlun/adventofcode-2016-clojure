7e0ce89cbfd22fe0b54e43804d904f1d3dfb254d
Emil Lundberg <lundberg.emil@gmail.com>
2017-04-04 02:08:12 +0200

    Add day06 solution

---
+++ src/adventofcode_2016/day06.clj
@@ -0,0 +1,28 @@
+(ns adventofcode-2016.day06
+  (:require clojure.string)
+  (:require [adventofcode-2016.util :refer (transpose)])
+)
+
+(defn solve-base [lines]
+  (->> lines
+    (transpose)
+    (map (fn [column]
+           (sort-by
+             (fn [ c ] (count (filter #(= c %) column)))
+             column
+           )))
+  ))
+
+(defn solve-a [lines]
+  (map last (solve-base lines)))
+
+(defn solve-b [lines]
+  (map first (solve-base lines)))
+
+(defn solve
+  "solve day 6 of Advent of Code 2016"
+  [input-lines & args]
+  (doseq [ [problem solver] [["Subproblem A:" solve-a] ["Subproblem B:" solve-b]] ]
+    (println problem (apply str (solver input-lines)))
+  )
+)
