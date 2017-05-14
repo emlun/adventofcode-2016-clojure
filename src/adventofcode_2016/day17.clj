e0e41e6bd375274d1c01b8903b5b8a4b524e471a
Emil Lundberg <lundberg.emil@gmail.com>
2017-05-14 22:04:36 +0200

    Solve day 17a

---
+++ src/adventofcode_2016/day17.clj
@@ -0,0 +1,138 @@
+(ns adventofcode-2016.day17
+  (:refer-clojure :exclude [resolve])
+  (:require clojure.string)
+  (:require [clojure.test :refer [is]])
+  (:require [adventofcode-2016.util :refer [bfs]])
+)
+
+(def ^{ :private true } MD5 (java.security.MessageDigest/getInstance "MD5"))
+
+(defn md5
+  { :test #(is (= "acbd" (md5 "foo"))) }
+  [s]
+    (->> s
+         (map byte)
+         (byte-array)
+         (.digest MD5)
+         (take 2)
+         (map #(format "%02x" %))
+         (apply str)
+         ))
+
+(defn represents-open? [c] (contains? #{\b \c \d \e \f} c))
+
+(defn open-state
+  { :test #(do
+             (is (=
+                  { :U false :D true :L true :R false }
+                  (open-state "foo" "RDLU")
+                  ))
+             (is (=
+                  { :U true :D true :L true :R false }
+                  (open-state "hijkl" [])
+                  ))
+             )}
+  [passcode path]
+  (let [
+        checksum (md5 (str passcode (apply str path)))
+        [ U D L R ] (map represents-open? checksum)
+        ]
+    { :U U, :D D, :L L, :R R }
+  )
+)
+
+(defn generate-next-states
+  { :test #(do
+             (is (=
+                  [
+                   {:pos {:x 0, :y -1}, :path [\U], :previous-door-open true}
+                   {:pos {:x 0, :y 1}, :path [\D], :previous-door-open true}
+                   {:pos {:x -1, :y 0}, :path [\L], :previous-door-open true}
+                   {:pos {:x 1, :y 0}, :path [\R], :previous-door-open false}
+                   ]
+                  (generate-next-states "hijkl" { :pos { :x 0, :y 0 }, :path [] })
+                  ))
+             )}
+  [passcode state]
+    (let [
+          options (open-state passcode (:path state))
+          ]
+      [
+       (-> state
+          (update-in [:pos :y] dec)
+          (update :path #(conj % \U))
+          (assoc :previous-door-open (:U options))
+          )
+       (-> state
+          (update-in [:pos :y] inc)
+          (update :path #(conj % \D))
+          (assoc :previous-door-open (:D options))
+          )
+       (-> state
+          (update-in [:pos :x] dec)
+          (update :path #(conj % \L))
+          (assoc :previous-door-open (:L options))
+          )
+       (-> state
+          (update-in [:pos :x] inc)
+          (update :path #(conj % \R))
+          (assoc :previous-door-open (:R options))
+          )
+      ]
+    )
+)
+
+(defn valid-state?
+  { :test #(do
+             (is (false? (valid-state? {} { :previous-door-open false })))
+             (is (false? (valid-state? { :x 0, :y 0 } { :previous-door-open true, :pos { :x 0, :y 0 } })))
+             (is (true? (valid-state? { :x 1, :y 1 } { :previous-door-open true, :pos { :x 0, :y 0 } })))
+             )}
+  [
+   map-dimensions
+   {
+    { :keys [x, y] } :pos
+    :keys [previous-door-open]
+   }
+  ]
+    (and
+      previous-door-open
+      (>= x 0)
+      (>= y 0)
+      (< x (:x map-dimensions))
+      (< y (:y map-dimensions))
+      ))
+
+(defn find-path
+  { :test #(do
+             (is (=
+               "DDRRRD"
+               (find-path { :x 4, :y 4 } "ihgpwlah")
+               ))
+             )}
+  [map-dimensions passcode]
+  (let [
+        target { :x (dec (:x map-dimensions))
+                 :y (dec (:y map-dimensions))
+                }
+        ]
+    (:path
+      (bfs {
+            :initial-state {
+                            :path []
+                            :pos { :x 0 :y 0 }
+                            :previous-door-open true
+                            }
+            :terminate? (fn [state] (= target (:pos state)))
+            :generate-next-states (partial generate-next-states passcode)
+            :skip? (fn [state history] (not (valid-state? map-dimensions state)))
+            })
+    )
+  )
+)
+
+(defn solve
+  "solve day 17 of Advent of Code 2016"
+  [input-lines & args]
+  (println "Shortest path: " (apply str (find-path { :x 4, :y 4 } (first input-lines))))
+)
